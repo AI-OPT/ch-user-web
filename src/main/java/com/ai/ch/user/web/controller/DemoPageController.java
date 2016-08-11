ee23a7ad5a741ec397d749dba4811b31e22573f5
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-08-11 10:43:01 +0800

    demo

---
+++ src/main/java/com/ai/ch/user/web/controller/DemoPageController.java
@@ -0,0 +1,20 @@
+package com.ai.ch.user.web.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoPageController {
+	private static final Log LOG = LogFactory.getLog(DemoPageController.class);
+    
+    @RequestMapping("/demopage")
+    public ModelAndView viewclc() {
+        return new ModelAndView("/demo/demopage");
+    }
+    
+
+}
