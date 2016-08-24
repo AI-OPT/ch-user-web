cbebd6a5417094ea466744ba9170fd52458a224b
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-08-24 14:11:43 +0800

    合同js

--- src/main/webapp/resources/spm_modules/app/jsp/contract/contract.js
+++ src/main/webapp/resources/spm_modules/app/jsp/contract/contract.js
@@ -15,7 +15,7 @@ define('app/jsp/contract/contract', function (require, exports, module) {
     require("my97DatePicker/WdatePicker");
     require("opt-paging/aiopt.pagination");
     require("twbs-pagination/jquery.twbsPagination.min");
-    
+    require("app/jsp/contract/ajaxfileupload");
     //实例化AJAX控制处理对象
     var ajaxController = new AjaxController();
     //定义页面组件类
@@ -38,6 +38,11 @@ define('app/jsp/contract/contract', function (require, exports, module) {
     		
     		"click [id='supplierSave']":"_saveSupplierContract",
     		"click [id='shopSave']":"_saveShopContract",
+    		
+    		/*"click [id='scanContract']":"_uploadFile",
+    		
+    		"change [id='scanFile']":"_uploadFile",*/
+    		
     		
         },
     	//重写父类
@@ -168,6 +173,47 @@ define('app/jsp/contract/contract', function (require, exports, module) {
 					});
 			}
 		},
+		_uploadFile:function(){
+			var scanContractFile = $("#scanFile").val();
+			$("#scanFileTest").val(scanContractFile);
+			var scanFileTest = $("#scanFileTest").val();
+			if(scanFileTest==""){
+				$("#scanContractErrMsg").show();
+				$("#scanContractText").show();
+				$("#scanContractText").text('扫描件合同不能为空');
+				$("#scanVersionContractFlag").val("0");
+				return false;
+			}else if(!/\.(PDF|PNG|JPG|DOC|pdf|png|jpg|doc)$/.test(scanFileTest)){
+				$("#scanContractErrMsg").show();
+				$("#scanContractText").show();
+				$("#scanContractText").text('文件格式不对，只允许上传pdf、png、jpg、word');
+				$("#scanVersionContractFlag").val("0");
+				return false;
+			}else if(scanFileTest.size>=(20.05*1024*1024)-1){
+				$("#scanContractErrMsg").show();
+				$("#scanContractText").show();
+				$("#scanContractText").text('文档太大，不能超过20M');
+				$("#scanVersionContractFlag").val("0");
+				return false;
+			}
+			 $.ajaxFileUpload({  
+		         url:_base+"/contract/uploadFile",  
+		         secureuri:false,  
+		         fileElementId:"scanFileTest",//file标签的id  
+		         dataType: 'json',//返回数据的类型  
+		         data:{scanFile:scanFileTest},//一同上传的数据  
+		         success: function (data, status) {
+		        	if(data.isTrue==true){
+		        		alert("dddd");
+		        		//document.getElementById(ddsId1).value=data.idpsId;
+		        		//$("#picFlag").val("1");
+		        	 }
+		         },
+		         error: function (data, status, e) {  
+		             alert(e);  
+		         }
+		     }); 
+		},
 		_saveShopContract:function(){
 			this._checkContractCodeValue();
 			this._checkContractNameValue();
@@ -224,3 +270,46 @@ define('app/jsp/contract/contract', function (require, exports, module) {
     });
     module.exports = ContractInfoPager
 });
+
+function uploadFile(fileId,inputText,errMsg,contractText,contractFlag){
+	var contractFile = $(fileId).val();
+	$("#"+inputText).val(contractFile);
+	var fileTest = $("#"+inputText).val();
+	if(fileTest==""){
+		$("#"+errMsg).show();
+		$("#"+contractText).show();
+		$("#"+contractText).text('扫描件合同不能为空');
+		$("#"+contractFlag).val("0");
+		return false;
+	}else if(!/\.(PDF|PNG|JPG|DOC|pdf|png|jpg|doc)$/.test(scanFileTest)){
+		$("#"+errMsg).show();
+		$("#"+contractText).show();
+		$("#"+contractText).text('文件格式不对，只允许上传pdf、png、jpg、word');
+		$("#"+contractFlag).val("0");
+		return false;
+	}else if(fileTest.size>=(20.05*1024*1024)-1){
+		$("#"+errMsg).show();
+		$("#"+contractText).show();
+		$("#"+contractText).text('文档太大，不能超过20M');
+		$("#"+contractFlag).val("0");
+		return false;
+	}
+	 $.ajaxFileUpload({  
+         url:_base+"/contract/uploadFile",  
+         secureuri:false,  
+         fileElementId:fileId,//file标签的id  
+         dataType: 'json',//返回数据的类型  
+         data:{"contractFileId":fileId},//一同上传的数据  
+         success: function (data, status) {
+        	if(data.isTrue==true){
+        		$("#"+contractFlag).val("1");
+        	 }else{
+        		 alert("上传失败");
+        		 $("#"+contractFlag).val("1");
+        	 }
+         },
+         error: function (data, status, e) {  
+             alert(e);  
+         }
+     }); 
+}
