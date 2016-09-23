576e5f6d33e4053fa55a81c0f2abb9e26ff39fb1
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-09-23 13:50:53 +0800

    解决编码问题

--- src/main/java/com/ai/ch/user/web/controller/DefaultManagerController.java
+++ src/main/java/com/ai/ch/user/web/controller/DefaultManagerController.java
@@ -1,18 +1,31 @@
 package com.ai.ch.user.web.controller;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +38,9 @@ import com.ai.ch.user.api.defaultlog.params.QueryDefaultLogRequest;
 import com.ai.ch.user.api.defaultlog.params.QueryDefaultLogResponse;
 import com.ai.ch.user.web.constants.ChWebConstants;
 import com.ai.ch.user.web.constants.ChWebConstants.ExceptionCode;
+import com.ai.ch.user.web.constants.TranType;
 import com.ai.ch.user.web.model.sso.client.GeneralSSOClientUser;
+import com.ai.ch.user.web.util.PayUtil;
 import com.ai.ch.user.web.vo.BusinessListInfo;
 import com.ai.opt.base.vo.PageInfo;
 import com.ai.opt.base.vo.ResponseHeader;
@@ -40,10 +55,28 @@ import com.ai.platform.common.api.sysuser.param.SysUserQueryResponse;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.upp.docking.covn.MsgString;
+import com.ylink.itfin.certificate.SecurityUtil;
+import com.ylink.upp.base.oxm.XmlBodyEntity;
+import com.ylink.upp.base.oxm.util.Dom4jHelper;
+import com.ylink.upp.base.oxm.util.HandlerMsgUtil;
+import com.ylink.upp.base.oxm.util.HeaderBean;
+import com.ylink.upp.base.oxm.util.MsgUtils;
+import com.ylink.upp.base.oxm.util.OxmHandler;
+
+import com.ylink.upp.oxm.entity.upp_100_001_01.PayOrderDetail;
+import com.ylink.upp.oxm.entity.upp_711_001_01.GrpBody;
+import com.ylink.upp.oxm.entity.upp_711_001_01.GrpHdr;
+import com.ylink.upp.oxm.entity.upp_711_001_01.ReqsInfo;
+
 
 @RestController
 @RequestMapping("/defaultManager")
 public class DefaultManagerController {
+	
+	/*@Autowired
+	private OxmHandler oxmHandler;*/
+	
 	
 	@RequestMapping("/defaultPager")
     public ModelAndView billingPager() {
@@ -86,12 +119,98 @@ public class DefaultManagerController {
 	
 	@RequestMapping("/addDefaultInfo")
 	public ModelAndView addDefaultInfo(HttpServletRequest request,String userId,String userName,String custName) {
+		/*//包装数据
+		GrpHdr hdr = new GrpHdr();
+		PayUtil payUtil = new PayUtil();
+		hdr.setMerNo("CO20160700000004");//设置一级平台商户号
+		hdr.setTranType(TranType.PAY_GUARANTEE_MONEY_QUERY.getValue());//设置交易类型(保证金支付订单查询)
+		
+		hdr.setCreDtTm(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+		GrpBody body = new GrpBody();
+		body.setSonMerNo("CO20160800000008");//设置要查询的二级商户编号
+		body.setResv("测试使用");//设置保留域
+		
+		ReqsInfo reqsInfo = new ReqsInfo();
+		reqsInfo.setGrpHdr(hdr);
+		reqsInfo.setGrpBody(body);
+		
+		// 发送消息
+		String xmlMsg = null;
+		try {
+			xmlMsg = oxmHandler.marshal(reqsInfo);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Map<String, String> param = new TreeMap<String, String>();
+		// 加签
+		String sign = null;
+		try {
+			sign = payUtil.sign(xmlMsg);
+			param.put("signMsg", sign);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 拼装报文头
+		String msgHeader = payUtil.initMsgHeader(hdr.getMerNo(), TranType.PAY_GUARANTEE_MONEY_QUERY.getValue());
+		param.put("msgHeader", msgHeader);
+		param.put("xmlBody", xmlMsg);
+		String result = null;
+		try {
+			result = payUtil.sendHttpPost("http://lytest.ngrok.tech:7777/upp-route/entry.html", param, "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		MsgString msgString = MsgUtils.patch(result);
+		String rh = msgString.getHeaderMsg();
+        String rb = msgString.getXmlBody();
+        String rs = msgString.getDigitalSign();
+
+        String balance = "0";//查询的金额
+		com.ylink.upp.base.oxm.XmlBodyEntity resultMsg = this.receiveMsg(rh, rb, rs);
+        com.ylink.upp.oxm.entity.upp_712_001_01.RespInfo receive = (com.ylink.upp.oxm.entity.upp_712_001_01.RespInfo)resultMsg;
+        if(receive == null){
+        	com.ylink.upp.oxm.entity.upp_599_001_01.RespInfo receive2 = (com.ylink.upp.oxm.entity.upp_599_001_01.RespInfo) resultMsg;
+            if(!"90000".equals(receive2.getGrpBody().getStsRsn().getRespCode())){
+            	throw new RuntimeException("系统异常.");
+            }
+        }else{
+            if(!"90000".equals(receive.getGrpBody().getStsRsn().getRespCode())){
+            	throw new RuntimeException("系统异常.");
+            }
+            balance=receive.getGrpBody().getStsRsnInf().getBalance();
+        }*/
 		Map<String, Object> model = new HashMap<String, Object>();
  		model.put("userId", userId);
- 		model.put("userName", userName);
- 		model.put("custName", custName);
+ 		try {
+			model.put("userName", URLDecoder.decode(userName,"utf-8"));
+			model.put("custName", URLDecoder.decode(custName,"utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+ 		//model.put("balance", Float.parseFloat(balance)/100);
+ 		model.put("balance", 2000);
 		return new ModelAndView("/jsp/defaultManager/addDefault",model);
 	}
+	
+	/*public XmlBodyEntity receiveMsg(String msgHeader, String xmlMsg, String sign) {
+		try {
+			PayUtil payUtil = new PayUtil();
+			boolean verify = payUtil.verify(xmlMsg, sign);
+			if (!verify) {
+				System.out.println("验签失败");
+			}
+			HeaderBean headerBean = new HeaderBean();
+			HandlerMsgUtil.conversion(msgHeader, headerBean);
+			xmlMsg = Dom4jHelper.addNamespace(xmlMsg, headerBean.getMesgType(), "UTF-8");
+			return (XmlBodyEntity) oxmHandler.unmarshaller(xmlMsg);
+		} catch (Exception e) {
+			System.out.println("接收数据时发生异常，错误信息为:" + e.getMessage());
+			throw new RuntimeException(e);
+		}
+
+	}*/
 	
 	@RequestMapping("/saveDefaultInfo")
 	@ResponseBody
@@ -115,6 +234,85 @@ public class DefaultManagerController {
 		    GeneralSSOClientUser userClient = (GeneralSSOClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
 			defaultLogRequest.setTenantId(userClient.getTenantId());
 			defaultLog.insertDefaultLog(defaultLogRequest);
+			
+			/*//调用长虹扣款
+			//包装数据
+			PayUtil payUtil = new PayUtil();
+			com.ylink.upp.oxm.entity.upp_100_001_01.GrpHdr hdr = new com.ylink.upp.oxm.entity.upp_100_001_01.GrpHdr();
+			hdr.setMerNo("CO20160700000004");//设置一级平台商户号
+			hdr.setTranType("100.001.01");//设置交易类型(保证金)
+			hdr.setCreDtTm(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+			
+			com.ylink.upp.oxm.entity.upp_100_001_01.GrpBody body = new com.ylink.upp.oxm.entity.upp_100_001_01.GrpBody();
+			String orderId = UUID.randomUUID().toString();
+			body.setMerOrderId(orderId);//设置要查询的二级商户编号
+			body.setUserName("uname");
+			body.setToken("token");
+			body.setOpenId("1");
+			body.setCustType("02");//企业
+			body.setPayCustNo("CB2016083000000004");
+			
+			BigDecimal orderAmt = new BigDecimal("0");
+			List<PayOrderDetail> details = new ArrayList<PayOrderDetail>();
+			PayOrderDetail detail = new PayOrderDetail();
+			detail.setProductAmt("1");//扣保证金金额（分）
+			detail.setProductName("productName1");//扣保证金原因
+			detail.setSonMerNo("CO20160700000006");//收取保证金商户号
+			detail.setMerSeqId(orderId);//商户主订单号
+			orderAmt = BigDecimal.valueOf(Long.parseLong(detail.getProductAmt()));//订单总金额(当前与扣款金额相同)
+			details.add(detail);
+
+			body.setOrderAmt(orderAmt.toString());
+			body.setOrderNum(details.size() + "");
+			body.setOrderDate(new SimpleDateFormat("yyyyMMdd").format(new Date()));
+			body.setPayOrderDetail(details);
+			body.setRemark("margindeposittest");
+
+			body.setNotifyUrl("http://chpay.ngrok.tech:7777/upp-simulation/pay/result/result.html");
+			body.setReturnUrl("http://chpay.ngrok.tech:7777/upp-simulation/pay/result/resultSuccess.html");
+			
+			body.setProductTypeName("margindeposit");
+			body.setResv("test");
+			body.setBizType("BAIL_BALANCE_PAY");// (保证金余额支付)
+			body.setResv("test");//设置保留域
+
+			
+			com.ylink.upp.oxm.entity.upp_100_001_01.ReqsInfo reqsInfo = new com.ylink.upp.oxm.entity.upp_100_001_01.ReqsInfo();
+			reqsInfo.setGrpHdr(hdr);
+			reqsInfo.setGrpBody(body);
+			
+			// 发送消息
+			String xmlMsg = null;
+			try {
+				xmlMsg = oxmHandler.marshal(reqsInfo);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			Map<String, String> param = new TreeMap<String, String>();
+			// 加签
+			String sign = null;
+			try {
+				ResourceLoader resourceLoader = new DefaultResourceLoader();
+				Resource pfxResource = resourceLoader.getResource("classpath:CO20160700000018.pfx"); 
+				InputStream in = new FileInputStream(pfxResource.getFile());
+				byte[] pfxByte = IOUtils.toByteArray(in);
+			    sign = SecurityUtil.pfxWithSign(pfxByte,xmlMsg, "111111");
+				param.put("signMsg", sign);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			// 拼装报文头
+			String msgHeader = payUtil.initMsgHeader(hdr.getMerNo(), "100.001.01");
+			param.put("msgHeader", msgHeader);
+			param.put("xmlBody", xmlMsg);
+			String result = null;
+			try {
+				result = payUtil.sendHttpPost("http://lytest.ngrok.tech:7777/upp-route/entry.html", param, "UTF-8");
+				System.out.println(result);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}*/
 			responseData = new ResponseData<String>(ExceptionCode.SUCCESS_CODE, "操作成功", null);
             responseHeader = new ResponseHeader(true,ExceptionCode.SUCCESS_CODE, "操作成功");
         }catch(Exception e){
@@ -130,8 +328,12 @@ public class DefaultManagerController {
 	public ModelAndView defaultHistoryPager(HttpServletRequest request,String userId,String userName,String custName) {
 		Map<String, Object> model = new HashMap<String, Object>();
  		model.put("userId", userId);
- 		model.put("userName", userName);
- 		model.put("custName", custName);
+ 		try {
+			model.put("userName", URLDecoder.decode(userName,"utf-8"));
+			model.put("custName", URLDecoder.decode(custName,"utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 		return new ModelAndView("/jsp/defaultManager/defaultHistoryList",model);
 	}
 	
