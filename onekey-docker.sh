1a990d78b60924dc0f61880b7cb9127a7c808401
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-10-09 14:37:29 +0800

    加空格

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -6,6 +6,6 @@ docker build -t 10.19.13.18:5000/ch-user-web:v1.0 .
 docker push 10.19.13.18:5000/ch-user-web:v1.0 
 
 docker rmi aioptapp/ch-user-web:v1.0
-docker tag 10.19.13.18:5000/ch-user-web:v1.0 aioptapp/ch-user-web:v1.0
+docker tag 10.19.13.18:5000/ch-user-web:v1.0  aioptapp/ch-user-web:v1.0
 docker login --username=aioptapp --password=aioptapp@123 --email=wuzhen3@asiainfo.com 
 docker push aioptapp/ch-user-web:v1.0
