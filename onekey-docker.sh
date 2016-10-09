eb4c3b92730e10fef739b00eaaf0a46cb7e1f295
gucl <gucl@asiainfo.com>
2016-10-09 19:20:06 +0800

    tagversion=v1.0

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -1,11 +1,12 @@
+tagversion=v1.0
 git reset --hard origin/master 
 git pull 
 chmod a+x onekey-docker.sh 
 gradle clean && gradle build -x test 
-docker build -t 10.19.13.18:5000/ch-user-web:v1.0 .   
-docker push 10.19.13.18:5000/ch-user-web:v1.0 
+docker build -t 10.19.13.18:5000/ch-user-web:${tagversion} .   
+docker push 10.19.13.18:5000/ch-user-web:${tagversion} 
 
-docker rmi aioptapp/ch-user-web:v1.0
-docker tag 10.19.13.18:5000/ch-user-web:v1.0  aioptapp/ch-user-web:v1.0
+docker rmi aioptapp/ch-user-web:${tagversion}
+docker tag 10.19.13.18:5000/ch-user-web:${tagversion}  aioptapp/ch-user-web:${tagversion}
 docker login --username=aioptapp --password=aioptapp@123 --email=wuzhen3@asiainfo.com 
-docker push aioptapp/ch-user-web:v1.0
+docker push aioptapp/ch-user-web:${tagversion}
