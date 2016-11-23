191a931e58ef8f0ed74274ae6406ec69e51c3954
zhangqiang7 <zhangqiang7@asiainfo.com>
2016-11-23 16:07:32 +0800

    修改

--- src/main/java/com/ai/ch/user/web/filter/AssembleUserInfoFilter.java
+++ src/main/java/com/ai/ch/user/web/filter/AssembleUserInfoFilter.java
@@ -139,13 +139,14 @@ public class AssembleUserInfoFilter implements Filter {
         if (ignor_suffix != null && ignor_suffix.length > 0) {
             String uri = req.getRequestURI().toLowerCase();
             for (String suffix : ignor_suffix) {
-                if (uri.endsWith(suffix)) {
+                if (uri.endsWith(suffix.toLowerCase())) {
                     return false;
                 }
             }
         }
         return true;
     }
+
     private boolean authMenu(HttpServletRequest request) {
     	
     	String currentURL = request.getRequestURI(); // 取得根目录所对应的绝对路径:
