aea2c238d1c9cc0f7ef79b9ee9056340f2248095
zhangqiang7 <zhangqiang7@asiainfo.com>
2016-11-28 17:13:25 +0800

    修改登出操作

--- src/main/java/com/ai/ch/user/web/controller/LogoutController.java
+++ src/main/java/com/ai/ch/user/web/controller/LogoutController.java
@@ -1,8 +1,5 @@
 package com.ai.ch.user.web.controller;
 
-
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -12,34 +9,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ai.ch.user.web.model.sso.client.GeneralSSOClientUser;
-import com.ai.opt.sso.client.filter.SSOClientConstants;
-import com.ai.opt.sso.client.filter.SSOClientUtil;
-
-
 @Controller
 public class LogoutController {
 	private static final Logger LOG = LoggerFactory.getLogger(LogoutController.class);
-	@RequestMapping("/ssologout")
-	public void logout(HttpServletRequest request,HttpServletResponse response){
+
+	@RequestMapping("/logout")
+	public void logout(HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession();
-		GeneralSSOClientUser user = (GeneralSSOClientUser) session.getAttribute(SSOClientConstants.USER_SESSION_KEY);
-		String logOutServerUrl = SSOClientUtil.getLogOutServerUrlRuntime(request);
-		String logOutBackUrl = SSOClientUtil.getLogOutBackUrlRuntime(request);
-		String casServerLoginUrl=SSOClientUtil.getCasServerLoginUrlRuntime(request);
-		String casServerUrlPrefix=SSOClientUtil.getCasServerUrlPrefixRuntime(request);
-		System.out.println("【LogoutController】casServerLoginUrl="+casServerLoginUrl);
-		System.out.println("【LogoutController】casServerUrlPrefix="+casServerUrlPrefix);
 		try {
-			if(user!=null){
-					session.removeAttribute(SSOClientConstants.USER_SESSION_KEY);
-					session.invalidate();
-			}
-			response.sendRedirect(logOutServerUrl + "?service=" + logOutBackUrl);
-		} catch (IOException e) {
-			LOG.error("用户登出失败",e);
+			session.invalidate();
+			session = null;
+		} catch (Exception e) {
+			LOG.error("用户登出失败", e);
 		}
-		
+
 	}
 
 }
